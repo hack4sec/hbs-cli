3ce0589b76f5843101976db67485fdf4eb0663d1
a.kuzmin <anton.kuzmin@marketgid.com>
2017-02-04 18:57:36 +0300

    refactoring HashlistsByAlgLoaderThread + unit-tests open

--- classes/HashlistsByAlgLoaderThread.py
+++ classes/HashlistsByAlgLoaderThread.py
@@ -14,13 +14,14 @@ import time
 
 from classes.Registry import Registry
 from classes.Factory import Factory
-from libs.common import _d, gen_random_md5, file_put_contents
+from libs.common import _d, gen_random_md5
 
 
 class HashlistsByAlgLoaderThread(threading.Thread):
     """ Thread for compile hashlists by common (one) alg """
     current_hashlist_id = None
     daemon = True
+    DELIMITER = 'UNIQUEDELIMITER'
 
     def __init__(self):
         threading.Thread.__init__(self)
@@ -35,8 +36,25 @@ class HashlistsByAlgLoaderThread(threading.Thread):
 
         self._db = Factory().new_db_connect()
 
-    def _hashlist_by_alg_exists(self, alg_id):
-        return self._db.fetch_one("SELECT id FROM hashlists WHERE common_by_alg = {0}".format(alg_id))
+    def _get_common_hashlist_id_by_alg(self, alg_id):
+        hashlist_id = self._db.fetch_one("SELECT id FROM hashlists WHERE common_by_alg = {0}".format(alg_id))
+        if hashlist_id is None:
+            alg_name = alg_name = self._db.fetch_one("SELECT name FROM algs WHERE id = {0}".format(alg_id))
+            hashlist_id = self._db.insert(
+                "hashlists",
+                {
+                    'name': 'All-{0}'.format(alg_name),
+                    'alg_id': alg_id,
+                    'have_salts': int(self._is_alg_have_salts(alg_id)),
+                    'delimiter': self.DELIMITER,
+                    'parsed': '0',
+                    'tmp_path': '',
+                    'status': 'ready',
+                    'when_loaded': int(time.time()),
+                    'common_by_alg': alg_id,
+                }
+            )
+        return hashlist_id
 
     def _get_current_work_hashlist(self):
         return self._db.fetch_one("SELECT hashlist_id FROM task_works WHERE status='work'")
@@ -45,108 +63,115 @@ class HashlistsByAlgLoaderThread(threading.Thread):
         return self._db.fetch_one("SELECT status FROM hashlists WHERE id = {0}".format(hashlist_id))
 
     def _is_alg_in_parse(self, alg_id):
-        return self._db.fetch_one(
+        result = self._db.fetch_one(
             "SELECT t.id FROM `task_works` t, `hashlists` hl "
             "WHERE t.hashlist_id = hl.id AND hl.alg_id = {0} "
             "AND t.status IN('waitoutparse','outparsing')".format(alg_id)
         )
+        return bool(result)
 
     def _hashes_count_in_hashlist(self, hashlist_id):
         return self._db.fetch_one("SELECT COUNT(id) FROM hashes WHERE hashlist_id = {0}".format(hashlist_id))
 
-    def run(self):
-        while True:
-            hashes_by_algs_count = self._db.fetch_pairs(
+    def _hashes_count_by_algs(self):
+        return self._db.fetch_pairs(
                 "SELECT hl.alg_id, COUNT(DISTINCT h.summ) FROM `hashes` h, hashlists hl "
-                "WHERE h.hashlist_id = hl.id AND !h.cracked AND hl.common_by_alg = 0 GROUP BY hl.alg_id"
+                "WHERE h.hashlist_id = hl.id AND !h.cracked AND hl.common_by_alg = 0 "
+                "GROUP BY hl.alg_id"
             )
 
-            for alg_id in hashes_by_algs_count:
-                hashlist_id = self._hashlist_by_alg_exists(alg_id)
+    def _is_alg_have_salts(self, alg_id):
+        return bool(
+            self._db.fetch_one(
+                "SELECT have_salts FROM hashlists WHERE alg_id = {0} ORDER BY have_salts DESC LIMIT 1".format(alg_id)
+            )
+        )
 
-                hashes_count_in_hashlist = self._hashes_count_in_hashlist(hashlist_id) if hashlist_id else 0
-                alg_have_salts = self._db.fetch_one(
-                    "SELECT have_salts FROM hashlists WHERE alg_id = {0} LIMIT 1".format(alg_id)
+    def _get_possible_hashlist_and_alg(self):
+        hashes_by_algs_count = self._hashes_count_by_algs()
+        for alg_id in hashes_by_algs_count:
+            if self._is_alg_in_parse(alg_id):
+                _d(
+                    "hashlist_common_loader",
+                    "Skip alg, it parsing or wait parse #{0}".format(
+                        alg_id
+                    )
                 )
+                continue
 
-                if hashes_count_in_hashlist != hashes_by_algs_count[alg_id]:
-                    _d(
-                        "hashlist_common_loader",
-                        "Build list for alg #{0} ({1} vs {2})".format(
-                            alg_id,
-                            hashes_count_in_hashlist,
-                            hashes_by_algs_count[alg_id]
-                        )
-                    )
+            hashlist_id = self._get_common_hashlist_id_by_alg(alg_id)
 
-                    if hashlist_id:
-                        if hashlist_id == self._get_current_work_hashlist() or \
+            if hashlist_id == self._get_current_work_hashlist() or \
                             self._get_hashlist_status(hashlist_id) != 'ready':
-                            _d(
-                                "hashlist_common_loader",
-                                "Skip it, it in work or not ready #{0}/{1}/{2}".format(
-                                    hashlist_id,
-                                    self._get_current_work_hashlist(),
-                                    self._get_hashlist_status(hashlist_id)
-                                )
-                            )
-                            continue
-
-                        if self._is_alg_in_parse(alg_id):
-                            _d(
-                                "hashlist_common_loader",
-                                "Skip alg, it parsing or wait parse #{0}".format(
-                                    alg_id
-                                )
-                            )
-                            continue;
-                        # Mark as 'parsing' for HashlistsLoader don`t get it to work before we done
-                        self._db.update("hashlists", {'parsed': 0, 'status': 'parsing'}, "id = {0}".format(hashlist_id))
-
-                    curs = self._db.q(
-                        "SELECT CONCAT(h.hash, 'UNIQUEDELIMITER', h.salt) as hash FROM hashes h, hashlists hl "
-                        "WHERE hl.id = h.hashlist_id AND hl.alg_id = {0} AND hl.common_by_alg = 0 AND !h.cracked".format(alg_id)
-                        if alg_have_salts else
-                        "SELECT h.hash FROM hashes h, hashlists hl "
-                        "WHERE hl.id = h.hashlist_id AND hl.alg_id = {0} AND hl.common_by_alg = 0 AND !h.cracked".format(alg_id)
+                _d(
+                    "hashlist_common_loader",
+                    "Skip it, it in work or not ready #{0}/{1}/{2}".format(
+                        hashlist_id,
+                        self._get_current_work_hashlist(),
+                        self._get_hashlist_status(hashlist_id)
                     )
+                )
+                continue
 
-                    if hashlist_id:
-                        _d("hashlist_common_loader", "Delete old hashes of #{0}".format(hashlist_id))
-                        self._db.q("DELETE FROM hashes WHERE hashlist_id = {0}".format(hashlist_id))
-                        self._db.q("UPDATE hashlists SET cracked=0, uncracked=0 WHERE id = {0}".format(hashlist_id))
+            hashes_count_in_hashlist = self._hashes_count_in_hashlist(hashlist_id)
 
-                    _d("hashlist_common_loader", "Put data in file for #{0}".format(hashlist_id))
-                    tmp_path = self.tmp_dir + "/" + gen_random_md5()
-                    for row in curs:
-                        file_put_contents(tmp_path, "{0}\n".format(row[0].strip()), True)
+            if hashes_count_in_hashlist == hashes_by_algs_count[alg_id]:
+                continue
 
-                    if not hashlist_id:
-                        alg_name = self._db.fetch_one("SELECT name FROM algs WHERE id = {0}".format(alg_id))
-                        hashlist_id = self._db.insert(
-                            "hashlists",
-                            {
-                                'name': 'All-{0}'.format(alg_name),
-                                'alg_id': alg_id,
-                                'have_salts': alg_have_salts,
-                                'delimiter': 'UNIQUEDELIMITER',
-                                'parsed': '0',
-                                'tmp_path': tmp_path,
-                                'status': 'wait',
-                                'when_loaded': '0',
-                                'common_by_alg': alg_id,
-                            }
-                        )
-                    else:
-                        self._db.update(
-                            "hashlists",
-                            {
-                                'parsed': '0',
-                                'status': 'wait',
-                                'tmp_path': tmp_path
-                            },
-                            "id = {0}".format(hashlist_id)
-                        )
-                    _d("hashlist_common_loader", "Done #{0}".format(hashlist_id))
+            _d(
+                "hashlist_common_loader",
+                "Build list for alg #{0} ({1} vs {2})".format(
+                    alg_id,
+                    hashes_count_in_hashlist,
+                    hashes_by_algs_count[alg_id]
+                )
+            )
+            return {'hashlist_id' : hashlist_id, 'alg_id' : alg_id}
+        return None
+
+    def _clean_old_hashes(self, hashlist_id):
+        self._db.q("DELETE FROM hashes WHERE hashlist_id = {0}".format(hashlist_id))
+        self._db.q("UPDATE hashlists SET cracked=0, uncracked=0 WHERE id = {0}".format(hashlist_id))
+
+    def _put_all_hashes_of_alg_in_file(self, alg_id):
+        curs = self._db.q(
+            "SELECT CONCAT(h.hash, '{0}', h.salt) as hash FROM hashes h, hashlists hl "
+            "WHERE hl.id = h.hashlist_id AND hl.alg_id = {1} AND hl.common_by_alg = 0 AND !h.cracked".format(
+                self.DELIMITER, alg_id)
+            if self._is_alg_have_salts(alg_id) else
+            "SELECT h.hash FROM hashes h, hashlists hl "
+            "WHERE hl.id = h.hashlist_id AND hl.alg_id = {0} AND hl.common_by_alg = 0 AND !h.cracked".format(alg_id)
+        )
+
+        tmp_path = self.tmp_dir + "/" + gen_random_md5()
+        fh = open(tmp_path, 'w')
+        for row in curs:
+            hash = row[0].strip()
+            if not len(hash) or hash == self.DELIMITER:
+                continue
+            fh.write("{0}\n".format(hash))
+        fh.close()
+
+        return tmp_path
+
+    def run(self):
+        while True:
+            candidate = self._get_possible_hashlist_and_alg()
+            if candidate is not None:
+                hashlist_id = candidate['hashlist_id']
+                alg_id = candidate['alg_id']
+
+                # Mark as 'parsing' for HashlistsLoader don`t get it to work before we done
+                self._db.update("hashlists", {'parsed': 0, 'status': 'parsing'}, "id = {0}".format(hashlist_id))
+
+                _d("hashlist_common_loader", "Delete old hashes of #{0}".format(hashlist_id))
+                self._clean_old_hashes(hashlist_id)
+
+                _d("hashlist_common_loader", "Put data in file for #{0}".format(hashlist_id))
+                tmp_path = self._put_all_hashes_of_alg_in_file(alg_id)
+
+                self._db.update("hashlists", {'status': 'wait', 'tmp_path': tmp_path}, "id = {0}".format(hashlist_id))
+
+                _d("hashlist_common_loader", "Done #{0}".format(hashlist_id))
 
             time.sleep(60)
---
+++ tests/unit/test_HashlistsByAlgLoaderThread.py
@@ -0,0 +1,283 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import configparser
+import os
+import time
+import pytest
+from pprint import pprint
+
+sys.path.append('../../')
+
+from libs.common import _d, file_get_contents, file_put_contents, md5
+from classes.Registry import Registry
+from classes.Database import Database
+from classes.HashlistsByAlgLoaderThread import HashlistsByAlgLoaderThread
+
+CURPATH = os.path.dirname(__file__) + "/"
+
+config = configparser.ConfigParser()
+config.read(CURPATH + 'config.ini')
+Registry().set('config', config)
+
+db = Database(
+    config['main']['mysql_host'],
+    config['main']['mysql_user'],
+    config['main']['mysql_pass'],
+    config['main']['mysql_dbname'],
+)
+Registry().set('db', db)
+
+
+class Test_HashlistsLoaderThread:
+    db = None
+    thrd = None
+
+    def setup_class(self):
+        self.db = Registry().get('db')  # type: Database
+
+    def setup(self):
+        self._clean_db()
+        self.thrd = HashlistsByAlgLoaderThread()
+
+    def teardown(self):
+        if isinstance(self.thrd, HashlistsByAlgLoaderThread):
+            del self.thrd
+        self._clean_db()
+
+    def _clean_db(self):
+        self.db.q("TRUNCATE TABLE dicts")
+        self.db.q("TRUNCATE TABLE dicts_groups")
+        self.db.q("TRUNCATE TABLE hashes")
+        self.db.q("TRUNCATE TABLE hashlists")
+        self.db.q("TRUNCATE TABLE rules")
+        self.db.q("TRUNCATE TABLE tasks")
+        self.db.q("TRUNCATE TABLE tasks_groups")
+        self.db.q("TRUNCATE TABLE task_works")
+
+    def _add_hashlist(self, id=1, name='test', alg_id=3, have_salts=0, status='ready', common_by_alg=0):
+        self.db.insert(
+            "hashlists",
+            {
+                'id': id,
+                'name': name,
+                'alg_id': alg_id,
+                'have_salts': have_salts,
+                'delimiter': '',
+                'cracked': 0,
+                'uncracked': 0,
+                'errors': '',
+                'parsed': 1,
+                'tmp_path': '',
+                'status': status,
+                'when_loaded': 0,
+                'common_by_alg': common_by_alg,
+            }
+        )
+
+    def _add_hash(self, hashlist_id=1, hash='', salt='', summ=''):
+        self.db.insert(
+            "hashes",
+            {
+                'hashlist_id': hashlist_id,
+                'hash': hash,
+                'salt': salt,
+                'password': '',
+                'cracked': 0,
+                'summ': summ
+            }
+        )
+
+    def test_get_common_hashlist_id_by_alg_get(self):
+        self._add_hashlist(have_salts=1, common_by_alg=3)
+        assert 1 == self.thrd._get_common_hashlist_id_by_alg(3)
+
+    def test_get_common_hashlist_id_by_alg_without_salt_create(self):
+        assert 1 == self.thrd._get_common_hashlist_id_by_alg(3)
+
+        test_hashlist_data = {'id': 1, 'name': 'All-MD4', 'have_salts': 0, 'delimiter': self.thrd.DELIMITER,
+                              'cracked': 0, 'uncracked': 0, 'errors': '', 'parsed': 0, 'status': 'ready',
+                              'common_by_alg': 3}
+        hashlist_data = self.db.fetch_row("SELECT * FROM hashlists WHERE id = 1")
+
+        for field in test_hashlist_data:
+            assert hashlist_data[field] == test_hashlist_data[field]
+
+    def test_get_common_hashlist_id_by_alg_with_salt_create(self):
+        self._add_hashlist(have_salts=1, common_by_alg=0)
+        self._add_hash(hash='a', salt='b', summ='333')
+
+        assert 2 == self.thrd._get_common_hashlist_id_by_alg(3)
+
+        test_hashlist_data = {'id': 2, 'name': 'All-MD4', 'have_salts': 1, 'delimiter': self.thrd.DELIMITER,
+                              'cracked': 0, 'uncracked': 0, 'errors': '', 'parsed': 0, 'status': 'ready',
+                              'common_by_alg': 3}
+        hashlist_data = self.db.fetch_row("SELECT * FROM hashlists WHERE id = 2")
+
+        for field in test_hashlist_data:
+            assert hashlist_data[field] == test_hashlist_data[field]
+
+    def test_get_common_hashlist_id_by_alg_with_salt_create_one_salt_forget(self):
+        self._add_hashlist(have_salts=1, common_by_alg=0)
+        self._add_hash(hash='a', salt='b', summ='333')
+
+        self._add_hashlist(id=2, have_salts=0, common_by_alg=0)
+        self._add_hash(hashlist_id=2, hash='c', salt='d', summ='111')
+
+        assert 3 == self.thrd._get_common_hashlist_id_by_alg(3)
+
+        test_hashlist_data = {'id': 3, 'name': 'All-MD4', 'have_salts': 1, 'delimiter': self.thrd.DELIMITER,
+                              'cracked': 0, 'uncracked': 0, 'errors': '', 'parsed': 0, 'status': 'ready',
+                              'common_by_alg': 3}
+        hashlist_data = self.db.fetch_row("SELECT * FROM hashlists WHERE id = 3")
+
+        for field in test_hashlist_data:
+            assert hashlist_data[field] == test_hashlist_data[field]
+
+    def test_get_current_work_hashlist(self):
+        assert not self.thrd._get_current_work_hashlist()
+        self.db.insert("task_works", {'hashlist_id': 3, 'status': 'work', 'task_id': 1})
+        assert 3 == self.thrd._get_current_work_hashlist()
+
+    def test_get_hashlist_status(self):
+        self._add_hashlist(common_by_alg=1)
+        assert 'ready' == self.thrd._get_hashlist_status(1)
+
+    def test_is_alg_in_parse(self):
+        assert False == self.thrd._is_alg_in_parse(3)
+        self._add_hashlist(common_by_alg=1)
+        self.db.insert("task_works", {'hashlist_id': 1, 'status': 'waitoutparse', 'task_id': 1})
+        assert True == self.thrd._is_alg_in_parse(3)
+
+        assert False == self.thrd._is_alg_in_parse(4)
+        self._add_hashlist(id=2, alg_id=4, common_by_alg=1)
+        self.db.insert("task_works", {'hashlist_id': 2, 'status': 'outparsing', 'task_id': 1})
+        assert True == self.thrd._is_alg_in_parse(4)
+
+    def test_hashes_count_in_hashlist(self):
+        assert 0 == self.thrd._hashes_count_in_hashlist(1)
+        self._add_hash()
+        assert 1 == self.thrd._hashes_count_in_hashlist(1)
+
+    def test_hashes_count_by_algs(self):
+        assert self.thrd._hashes_count_by_algs() == {}
+
+        self._add_hashlist()
+        self._add_hash(summ='111')
+        self._add_hash(summ='222', hash='a', salt='b')
+
+        self._add_hashlist(id=2, alg_id=4)
+        self._add_hash(hashlist_id=2, summ='333')
+
+        assert self.thrd._hashes_count_by_algs() == {3: 2, 4: 1}
+
+    def test_is_alg_have_salts(self):
+        self._add_hashlist()
+        assert False == self.thrd._is_alg_have_salts(3)
+
+        self._add_hashlist(id=2, have_salts=1) # Forget salt bug
+        assert True == self.thrd._is_alg_have_salts(3)
+
+    def test_get_possible_hashlist_and_alg_simple(self):
+        self._add_hashlist()
+        self._add_hash(hash='a', summ='111')
+        self._add_hash(hash='b', summ='222')
+        assert {'hashlist_id': 2, 'alg_id': 3} == self.thrd._get_possible_hashlist_and_alg()
+
+    def test_get_possible_hashlist_and_alg_none_already(self):
+        self._add_hashlist()
+        self._add_hash(hash='a', summ='111')
+        self._add_hash(hash='b', summ='222')
+
+        self._add_hashlist(id=2, common_by_alg=3)
+        self._add_hash(hashlist_id=2, hash='a', summ='111')
+        self._add_hash(hashlist_id=2, hash='b', summ='222')
+
+        assert None == self.thrd._get_possible_hashlist_and_alg()
+
+    def test_get_possible_hashlist_and_alg_none_in_parse(self):
+        self.db.insert("task_works", {'hashlist_id': 1, 'status': 'waitoutparse', 'task_id': 1})
+
+        self._add_hashlist()
+        self._add_hash(hash='a', summ='111')
+        self._add_hash(hash='b', summ='222')
+
+        assert None == self.thrd._get_possible_hashlist_and_alg()
+
+        self.db.update("task_works", {'status': 'outparsing'}, "id=1")
+
+        assert None == self.thrd._get_possible_hashlist_and_alg()
+
+    def test_get_possible_hashlist_and_alg_none_not_ready(self):
+        self._add_hashlist()
+        self._add_hash(hash='a', summ='111')
+        self._add_hash(hash='b', summ='222')
+
+        self._add_hashlist(id=2, status='wait', common_by_alg=3)
+
+        assert None == self.thrd._get_possible_hashlist_and_alg()
+
+    def test_get_possible_hashlist_and_alg_none_in_work(self):
+        self._add_hashlist()
+        self._add_hash(hash='a', summ='111')
+        self._add_hash(hash='b', summ='222')
+
+        self._add_hashlist(id=2, common_by_alg=3)
+
+        self.db.insert("task_works", {'hashlist_id': 2, 'status': 'work', 'task_id': 1})
+
+        assert None == self.thrd._get_possible_hashlist_and_alg()
+
+    def test_clean_old_hashes(self):
+        self._add_hashlist()
+        self._add_hash(hash='a', summ='111')
+        self._add_hash(hash='b', summ='222')
+
+        assert 2 == self.db.fetch_one("SELECT COUNT(*) FROM hashes WHERE hashlist_id = 1")
+
+        self.thrd._clean_old_hashes(1)
+
+        assert 0 == self.db.fetch_one("SELECT COUNT(*) FROM hashes WHERE hashlist_id = 1")
+        assert 0 == self.db.fetch_one("SELECT cracked+uncracked FROM hashlists WHERE id = 1")
+
+    def test_put_all_hashes_of_alg_in_file(self):
+        self._add_hashlist()
+        self._add_hash(hash='a', summ='111')
+        self._add_hash(summ='222')
+        self._add_hash(hash='b', summ='333')
+
+        path = self.thrd._put_all_hashes_of_alg_in_file(3)
+
+        assert os.path.exists(path)
+        assert file_get_contents(path) == 'a\nb\n'
+
+        self._add_hashlist(id=2, have_salts=1, alg_id=4)
+        self._add_hash(hashlist_id=2, hash='a', salt='b', summ='111')
+        self._add_hash(hashlist_id=2, summ='222')
+        self._add_hash(hashlist_id=2, hash='c', salt='d', summ='333')
+
+        path = self.thrd._put_all_hashes_of_alg_in_file(4)
+
+        assert os.path.exists(path)
+        assert file_get_contents(path) == 'a{0}b\nc{0}d\n'.format(self.thrd.DELIMITER)
+
+    def test_simple_one_run(self):
+        self._add_hashlist()
+        self._add_hash(hash='a', summ='111')
+        self._add_hash(hash='b', summ='222')
+
+        assert None == self.db.fetch_one("SELECT id FROM hashlists WHERE common_by_alg")
+
+        self.thrd.start()
+        time.sleep(5)
+
+        test_hashlist_data = {'id': 2, 'name': 'All-MD4', 'have_salts': 0, 'delimiter': self.thrd.DELIMITER,
+                              'cracked': 0, 'uncracked': 0, 'errors': '', 'parsed': 0, 'status': 'wait',
+                              'common_by_alg': 3}
+        hashlist_data = self.db.fetch_row("SELECT * FROM hashlists WHERE common_by_alg")
+
+        for field in test_hashlist_data:
+            assert hashlist_data[field] == test_hashlist_data[field]
+
+
+
