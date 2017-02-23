2127178c7a2446e259e6903e29e4280e4a6a294a
a.kuzmin <anton.kuzmin@marketgid.com>
2017-02-23 10:45:21 +0300

    Work thread for finder.insidepro API + tests

--- database-from_0.2a_to_0.3a.sql
+++ database-from_0.2a_to_0.3a.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `hashlists` CHANGE `common_by_alg` `common_by_alg` SMALLINT UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `hashlists` ADD `last_finder_checked` INT NOT NULL DEFAULT '0' AFTER `common_by_alg`;
+ALTER TABLE `algs` ADD `finder_insidepro_allowed` BOOLEAN NOT NULL DEFAULT FALSE AFTER `alg_id`;
